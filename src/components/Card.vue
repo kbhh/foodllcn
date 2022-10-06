7b5453620158e16b378da91f4f03e7c1c0c53e16
kbhh <kbromhailay0@gmail.com>
2022-10-06 20:28:31 +0200

    changed background

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,9 @@
 <template>
   <b-container>
-    <div class="border py-3 mx-1  my-2 px-2 py-2" >
+    <div class="border py-3 mx-1 bg-secondary   my-2 px-2 py-2" >
         <h5 class="text-center"> {{menu.title}}</h5>
         <p >{{menu.desc}}{{menu.desc}}{{menu.desc}}{{menu.desc}}</p>
         <p >{{menu.desc}}{{menu.desc}}{{menu.desc}}{{menu.desc}}</p>
-
       </div>
   </b-container>
 </template>
